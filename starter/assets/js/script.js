6c64f0a7f8e66afb44bf4e24e0fb316ffa9fecf2
angelica-zach <angelscakeunicorn@gmail.com>
2023-12-11 17:02:51 +0000

    added save functionality

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,7 +1,6 @@
 var currentDayEl=$("#currentDay");
 var currentHour="";
 var hourEl= $("#hour");
-var description=$(".textarea");
 var timeblock=$(".row");
 // handle displaying the day
 function displayTime() {
@@ -28,7 +27,21 @@ function displayTime() {
         };
       });
     };
+    // save button event listener
+$(".saveBtn").on("click", function() {
+  var textinput = $(this).siblings(".description");
+  var inputValue = textinput.val();
+  var time = $(this).siblings(hourEl).text();
+  localStorage.setItem(time, inputValue)
+});
+// loading values using the key of hour
+timeblock.each(function() {
+  var key = $(this).find(hourEl).text();
+  var savedInput = localStorage.getItem(key);
 
-
+  if (savedInput !== null) {
+    $(this).find(".description").val(savedInput);
+  }
+});
 // making sure time is regularly updated
   setInterval(displayTime, 1000);
